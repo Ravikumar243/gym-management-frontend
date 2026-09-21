e1d71ba68b6dd5e00c2b846342bf8d26c4791043
Ravikumar243 <rkmahawar04@gmail.com>
2026-09-21 21:17:44 +0530

    changes are done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import menuItems from "../../config/menuConfig";
+import menuItems from "../../config/MenuConfig";
 
 import "./Sidebar.css";
 
