449edcc1757510f15fbbc16017d2cb01867b3f8b
Ravikumar243 <rkmahawar04@gmail.com>
2026-09-21 21:43:57 +0530

    Url change done

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,3 +1,4 @@
-const BASE_URL = "http://localhost:3000/api/v1"
+// const BASE_URL = "http://localhost:3000/api/v1"
+const BASE_URL = "https://gym-management-backend-7mdl.onrender.com/api/v1"
 
 export default BASE_URL
